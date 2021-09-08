c2938330b496b4837c0034ca0762f4c41e47d886
Karolaine <karolbt04@gmail.com>
2021-09-08 09:55:43 -0300

    adicionando os arquivos referente ao banco

--- meuBanco/Program.cs
+++ meuBanco/Program.cs
@@ -6,7 +6,22 @@ namespace meuBanco
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Mensagem importante!");
+            ContaCorrente contaDaKarol = new ContaCorrente();
+
+            contaDaKarol.titular = "Karol";
+            contaDaKarol.agencia = 1234;
+            contaDaKarol.conta = 789;
+            contaDaKarol.saldo = 100.50;
+
+            ContaCorrente contaDoKaique = new ContaCorrente();
+
+            contaDoKaique.titular = "Kaique";
+            contaDoKaique.agencia = 4321;
+            contaDoKaique.conta = 987;
+            contaDoKaique.saldo = 100.50;
+
+            Console.WriteLine("O titular da conta é: " +contaDaKarol.titular);
+            Console.WriteLine("O titular da conta é: " +contaDoKaique.titular);
         }
     }
 }
