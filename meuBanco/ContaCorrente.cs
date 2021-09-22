6b31785f3ae0538734476895e3af6ce9780d1c97
Karolaine <karolbt04@gmail.com>
2021-09-22 10:25:52 -0300

    atualizando a conta corrente pública para privada

--- meuBanco/ContaCorrente.cs
+++ meuBanco/ContaCorrente.cs
@@ -1,51 +1,63 @@
 public class ContaCorrente{
-    public string titular;
-    public int agencia;
-    public int conta;
-    public double saldo;
+    public string titular {get; set;}
+    public int agencia {get; set;}
+    public int conta {get; set;}
+    private double saldo;
 
-    public ContaCorrente(){}
+    public double Saldo
+    {
+        get
+        {
+            return this.saldo;
+        }
+        set
+        {
+            if (value >= 0)
+            {
+                this.saldo = value;
+            }
+        }
+    }
 
-    public ContaCorrente(string contacorrente_titular, int contacorrente_agencia, int contacorrente_conta, double contacorrente_saldo) { 
+    public ContaCorrente() {}
+
+    public ContaCorrente(string contacorrente_titular, int contacorrente_agencia, int contacorrente_conta, double contacorrente_saldo) 
+    { 
         titular = contacorrente_titular;
-        agencia = contacorrente_agencia; 
-        conta = contacorrente_conta; 
+        agencia = contacorrente_agencia;
         saldo = contacorrente_saldo; 
     } 
 
-        public bool Sacar (double valor){ 
-
-            if(this.saldo < valor){ 
-                return false; 
-            } 
-
-                else{ 
-                    this.saldo -= valor; 
-                    return true; 
-                } 
-
-        }
-
-        public void Depositar (double valor){ 
-
-            this.saldo += valor; 
-            
+    public bool Sacar (double valor)
+    { 
+        if(this.saldo < valor)
+        { 
+            return false; 
         } 
-        
-        public bool Transferir (double valor, ContaCorrente contaDestino){ 
-            
-            if (this.saldo < valor){ 
-                
-                return false; 
-            
-            }
-
-            else{ 
-
-                 this.Sacar(valor); 
-                 contaDestino.Depositar(valor); 
-                 return true; 
-                 
-            } 
+        else
+        { 
+            this.saldo -= valor; 
+            return true; 
         } 
     }
+
+    public void Depositar (double valor)
+    { 
+        this.saldo += valor; 
+    } 
+        
+    public bool Transferir (double valor, ContaCorrente contaDestino)
+    {     
+        if (this.saldo < valor)
+        { 
+            return false; 
+        }
+
+        else
+        { 
+            this.Sacar(valor); 
+            contaDestino.Depositar(valor); 
+            return true; 
+        } 
+    } 
+}
--- meuBanco/Program.cs
+++ meuBanco/Program.cs
@@ -7,24 +7,25 @@ namespace meuBanco
         static void Main(string[] args)
         {
             ContaCorrente contaDaKarol = new ContaCorrente("Karol", 1234, 789, 100.50);
+            
+            contaDaKarol.conta = 890;
 
             ContaCorrente contaDoKaique = new ContaCorrente("Kaique", 4321, 987, 120);
 
             ContaCorrente contaDaKatlin = new ContaCorrente("Katlin", 4322, 988, 150);
 
-            Console.WriteLine("O titular da conta é: " +contaDaKarol.titular);
+            contaDaKarol.Sacar(100);
+
+            /*Console.WriteLine("O titular da conta é: " +contaDaKarol.titular);
             Console.WriteLine("O titular da conta é: " +contaDoKaique.titular);
             Console.WriteLine("O titular da conta é: " +contaDaKatlin.titular);
 
-            bool retorno = contaDaKarol.Sacar(50);
-            //Console.WriteLine(retorno);
-            Console.WriteLine("O saldo da conta da Karol é: " + contaDaKarol.saldo);
-
             contaDaKarol.Depositar(150);
-            Console.WriteLine("O saldo da conta da Karol é: " + contaDaKarol.saldo);
+            contaDaKarol.Transferir(50, contaDoKaique);*/
 
-            contaDaKarol.Transferir(50, contaDoKaique);
+            Console.WriteLine("O número da conta da Karol é: "+ contaDaKarol.conta);
             Console.WriteLine("O saldo da conta da Karol é: " + contaDaKarol.saldo);
+            Console.WriteLine("O número da conta do Kaique é: " + contaDoKaique.conta);
             Console.WriteLine("O saldo da conta do Kaique é: " + contaDoKaique.saldo);
 
         }
